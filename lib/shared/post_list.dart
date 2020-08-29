d96a86a8a418c5569328a02d4f04a5c845929687
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-08-29 20:02:09 +0100

    widget class for all post list

---
+++ lib/shared/post_list.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:techno_vile_blog/models/post_model.dart';
+import 'package:techno_vile_blog/widgets/post_tile.dart';
+
+class PostList extends StatefulWidget {
+  @override
+  _PostListState createState() => _PostListState();
+}
+
+class _PostListState extends State<PostList> {
+  @override
+  Widget build(BuildContext context) {
+    final post = Provider.of<List<PostModel>>(context) ?? [];
+    return ListView.builder(
+      primary: false,
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: post.length,
+      itemBuilder: (_, index) {
+        return PostTile(
+          posts: post[index],
+        );
+      },
+    );
+  }
+}
