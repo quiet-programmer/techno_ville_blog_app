8d14d01d43b7254c37a302feb00db943aabc6c7d
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-09-01 15:56:12 +0100

    update app theme design

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:techno_vile_blog/const_value.dart';
-import 'package:techno_vile_blog/screens/wrapper.dart';
+import 'package:techno_vile_blog/auth/wrapper.dart';
 import 'package:techno_vile_blog/services/auth.dart';
+import 'package:techno_vile_blog/widgets/messaging_widget.dart';
 
 class App extends StatefulWidget {
   @override
@@ -10,6 +11,18 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+    MessagingWidget();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    MessagingWidget();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
