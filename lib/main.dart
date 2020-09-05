1c2ccd3a33f9d008e3d16bc9475428a54fb7425b
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-09-05 14:12:54 +0100

    working on the push notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:techno_vile_blog/src/app.dart';
+import 'package:workmanager/workmanager.dart';
 
-void main() {
+//this is the name given to the background fetch
+const simplePeriodicTask = "simplePeriodicTask";
+
+// flutter local notification setup
+void showNotification(v, flp) async {
+  var android = AndroidNotificationDetails(
+    'channel id',
+    'channel NAME',
+    'CHANNEL DESCRIPTION',
+    priority: Priority.High,
+    importance: Importance.Max,
+  );
+  var iOS = IOSNotificationDetails();
+  var platform = NotificationDetails(android, iOS);
+  await flp.show(
+    0,
+    'Virtual intelligent solution',
+    '$v',
+    platform,
+    payload: 'VIS \n $v',
+  );
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Workmanager.initialize(callbackDispatcher, isInDebugMode: true);
+  await Workmanager.registerPeriodicTask(
+    "5",
+    simplePeriodicTask,
+    existingWorkPolicy: ExistingWorkPolicy.replace,
+    frequency: Duration(minutes: 15),
+    initialDelay: Duration(seconds: 5),
+    constraints: Constraints(
+      networkType: NetworkType.connected,
+    ),
+  );
   runApp(App());
+}
+
+void callbackDispatcher() {
+  Workmanager.executeTask((taskName, inputData) async {
+    FlutterLocalNotificationsPlugin flp = FlutterLocalNotificationsPlugin();
+
+    var android = AndroidInitializationSettings('@mipmap/ic_launcher');
+    var ios = IOSInitializationSettings();
+    var initSettings = InitializationSettings(android, ios);
+    flp.initialize(initSettings);
+
+    //TODOfix the notification settinngs
+    showNotification("Love me more", flp);
+
+    return Future.value(true);
+  });
 }
