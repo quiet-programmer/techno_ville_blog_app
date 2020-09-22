f5f3b8bf90ad78829c362ed32cdd85f6f49831f5
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-09-22 21:39:24 +0100

    added new values

--- lib/const_value.dart
+++ lib/const_value.dart
@@ -2,3 +2,5 @@ import 'package:flutter/cupertino.dart';
 
 const Color realColor = Color.fromRGBO(50, 129, 222, 1);
 const Color subColor = Color.fromRGBO(0, 0, 0, 1);
+
+const Color containerColor = Color.fromRGBO(42, 44, 49, 1);
