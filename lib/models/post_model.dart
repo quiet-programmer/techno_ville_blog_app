d6074678da2f6b1eab50d31aeb408ac591a071f5
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-10-29 09:31:15 +0100

    added new params

--- lib/models/post_model.dart
+++ lib/models/post_model.dart
@@ -1,11 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class PostModel {
+  final String docId;
   final String title;
   final String authur;
+  final String image;
   final String article;
+  final String comments;
+  final Timestamp time;
 
   PostModel({
+    this.docId,
     this.title,
     this.authur,
+    this.image,
     this.article,
+    this.comments,
+    this.time,
   });
 }
