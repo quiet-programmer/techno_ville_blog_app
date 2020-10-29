22d73b414783f82453f2b566b26d8849b77fe48b
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-10-29 09:32:16 +0100

    little changes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:techno_vile_blog/const_value.dart';
-import 'package:techno_vile_blog/services/auth.dart';
+import 'package:techno_vile_blog/models/post_model.dart';
+import 'package:techno_vile_blog/services/database.dart';
+import 'package:techno_vile_blog/shared/post_list.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,16 +11,123 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final AuthServices _userAuth = AuthServices();
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: subColor,
-      body: Center(
-        child: Text(
-          "Welcome Home",
-          style: TextStyle(
-            color: Colors.white,
+    return StreamProvider<List<PostModel>>.value(
+      value: DatabaseService().post,
+      child: Scaffold(
+        backgroundColor: subColor,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+            child: Column(
+              children: [
+                Container(
+                  height: 160.0,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      Container(
+                        height: 150.0,
+                        width: 150.0,
+                        padding: const EdgeInsets.only(bottom: 5.0),
+                        decoration: BoxDecoration(
+                          color: containerColor,
+                          borderRadius: BorderRadius.circular(10.0),
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/techno.png'),
+                          ),
+                        ),
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Text(
+                            "Meet Writters",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Container(
+                        height: 150.0,
+                        width: 150.0,
+                        padding: const EdgeInsets.only(bottom: 5.0),
+                        decoration: BoxDecoration(
+                          color: containerColor,
+                          borderRadius: BorderRadius.circular(10.0),
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/techno.png'),
+                          ),
+                        ),
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Text(
+                            "See Events",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Container(
+                        height: 150.0,
+                        width: 150.0,
+                        padding: const EdgeInsets.only(bottom: 5.0),
+                        decoration: BoxDecoration(
+                          color: containerColor,
+                          borderRadius: BorderRadius.circular(10.0),
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/techno.png'),
+                          ),
+                        ),
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Text(
+                            "Drop a request",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 40.0,
+                ),
+                Text(
+                  "All Articles",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                PostList(),
+              ],
+            ),
           ),
         ),
       ),
