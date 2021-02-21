3a9ca9845e77026075060c247e070b1690cfb624
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-02-21 09:59:49 +0100

    little changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:techno_vile_blog/const_value.dart';
+import 'package:provider/provider.dart';
+import 'package:techno_vile_blog/provider/theme_provider.dart';
+import 'package:techno_vile_blog/services/auth.dart';
 import 'package:techno_vile_blog/src/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    systemNavigationBarColor: subColor,
-  ));
+  // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+  //   systemNavigationBarColor: backColor,
+  // ));
 
-  runApp(App());
+  runApp(
+    MultiProvider(
+      providers: [
+        StreamProvider.value(
+          value: AuthServices().user,
+        ),
+        ChangeNotifierProvider.value(
+          value: ThemeProvider(),
+        ),
+      ],
+      child: App(),
+    ),
+  );
 }
