c8e298ad0e37dd66ca3d499fa4cba0b2e278ed26
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-03-06 22:30:28 +0100

    added new values and new changes

--- lib/const_value.dart
+++ lib/const_value.dart
@@ -1,6 +1,47 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:techno_vile_blog/utils/hex_to_color.dart';
 
 const Color realColor = Color.fromRGBO(50, 129, 222, 1);
-const Color subColor = Color.fromRGBO(0, 0, 0, 1);
+// const Color subColor = Color.fromRGBO(0, 0, 0, 1);
+Color backColor = Colors.grey[900];
+Color backColorOne = Colors.grey[100];
+Color subColor = Colors.grey[850];
 
 const Color containerColor = Color.fromRGBO(42, 44, 49, 1);
+const Color boxColor = Color.fromRGBO(235, 237, 242, 1);
+
+Color textLight = hexToColor("#ffffff");
+Color textDark = hexToColor("#0e0e0f");
+
+Widget lightTabLogo() {
+  return Image.asset(
+    'assets/images/techno_vile.png',
+    width: 200.0,
+    height: 150.0,
+  );
+}
+
+Widget lightLogo() {
+  return Image.asset(
+    'assets/images/techno.png',
+    width: 150.0,
+    height: 150.0,
+  );
+}
+
+Widget darkTabLogo() {
+  return Image.asset(
+    'assets/images/techno_vile_dark.png',
+    width: 200.0,
+    height: 150.0,
+  );
+}
+
+Widget darkLogo() {
+  return Image.asset(
+    'assets/images/techno_dark.png',
+    width: 150.0,
+    height: 150.0,
+  );
+}
