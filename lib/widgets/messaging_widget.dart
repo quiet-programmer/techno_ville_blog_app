ee5a055b398b2c6245adf2dd23947c3512bcc25c
Godsend Joseph <bobbyjoseph108@gmail.com>
2021-05-31 23:32:41 +0100

    removed obsolete code

--- lib/widgets/messaging_widget.dart
+++ lib/widgets/messaging_widget.dart
@@ -8,44 +8,11 @@ class MessagingWidget extends StatefulWidget {
 }
 
 class _MessagingWidgetState extends State<MessagingWidget> {
-  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
-
   final List<MessageModel> messages = [];
 
   @override
   void initState() {
     super.initState();
-    _firebaseMessaging.configure(
-      onMessage: (Map<String, dynamic> message) async {
-        print("onMessage $message");
-        final notification = message['notification'];
-        setState(() {
-          messages.add(
-            MessageModel(
-              title: notification['title'],
-              body: notification['body'],
-            ),
-          );
-        });
-      },
-      onBackgroundMessage: (Map<String, dynamic> message) async {},
-      onLaunch: (Map<String, dynamic> message) async {
-        print("onMessage $message");
-        final notification = message['data'];
-        setState(() {
-          messages.add(MessageModel(
-            title: '${notification['title']}',
-            body: '${notification['body']}',
-          ));
-        });
-      },
-      onResume: (Map<String, dynamic> message) async {
-        print("onMessage $message");
-      },
-    );
-    _firebaseMessaging.requestNotificationPermissions(
-      const IosNotificationSettings(sound: true, badge: true, alert: true),
-    );
   }
 
   Widget buildMessage(MessageModel messageModel) {
